eca34098da2606273b1bd4ad60352951b2c59793
Krisztina Pap <krisztina.pap@outlook.com>
2020-10-05 16:26:01 -0600

    Adds search form; Adds styling;

--- index.php
+++ index.php
@@ -1,42 +1,56 @@
 <?php
 include './templates/header.php';
 
-// @link https://github.com/workforce-data-initiative/skills-api/wiki/API-Overview
-$skillsAPIResponse = file_get_contents( 'http://api.dataatwork.org/v1/jobs' );
-
-if ( $skillsAPIResponse )
-{
-    $skillsData = json_decode( $skillsAPIResponse );
-    if ( $skillsData )
-    {
-        foreach ( $skillsData as $skill )
-        {
-            if ( isset($skill->title) )
-            {
-            ?>
-                <li>
-                    <?php echo $skill->title; ?>
-                </li>
-            <?php
-            }
-          
-        }
-    }
-    else
-    {
-        echo "ERROR: Unable to convert API response to PHP.";
-    }
-} 
-else
-{
-    echo "ERROR: Unable to get API response.";
-}
 ?>
 
 <h1>Krisztina's PHP API Practice</h1>
 
+<div class="content-area">
 
+<h2>Open Skills API - Job titles</h2>
 
+<form action="#" method="GET">
+    <label for="keyword">Enter a keyword:
+        <input type="text" id="keyword" name="keyword">
+    </label>
+    <input type="submit" value="Search">
+</form>
+
+<ul>
+    <?php
+    // @link https://github.com/workforce-data-initiative/skills-api/wiki/API-Overview
+    $skillsAPIResponse = file_get_contents( "http://api.dataatwork.org/v1/jobs/autocomplete?contains={$_GET['keyword']}" );
+
+    if ( $skillsAPIResponse )
+    {
+        $skillsData = json_decode( $skillsAPIResponse );
+        if ( $skillsData )
+        {
+            foreach ( $skillsData as $skill )
+            {
+                if ( isset($skill->normalized_job_title) )
+                {
+                ?>
+                    <li>
+                        <?php echo $skill->normalized_job_title; ?>
+                    </li>
+                <?php
+                }  
+            }
+        }
+        else
+        {
+            echo "ERROR: Unable to convert API response to PHP.";
+        }
+    } 
+    else
+    {
+        echo "ERROR: Unable to get API response.";
+    }
+
+    ?>
+</ul>
+</div>
 <?php
     include './templates/footer.php';
 ?>
